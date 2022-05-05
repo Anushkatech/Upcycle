289e635c8a34045f6e03813dbc5d0fe6f2b96657
Anushkatech <104856348+Anushkatech@users.noreply.github.com>
2022-05-05 13:40:07 +0530

    Added form saving functionality

--- script.js
+++ script.js
@@ -1,13 +1,102 @@
 const panels = document.querySelectorAll('.panel')
-panels.forEach((panel) =>{
-    panel.addEventListener('click',() =>{
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
 
-function removeActiveClasses(){
+function removeActiveClasses() {
     panels.forEach((panel) => {
         panel.classList.remove('active')
     })
 }
+
+function eneterDataIntoSheet() {
+
+}
+
+//Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+function openModal() {
+    modal.style.display = "flex";
+    document.body.style.overflow = "hidden";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto";
+    }
+}
+
+var request;
+
+$("#register-form").submit(function (event) {
+    event.preventDefault()
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    // $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycby50fy0aUAi7THLP6t5r5VDgQvvVUl3idAzCYyC2BNeaeqABl25-A6z10s975cXgYbUQQ/exec",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR) {
+        alert("Thank You, your details have been saved!");
+        $("#register-form")[0].reset();
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+        // Log the error to the console
+        console.error(
+            "The following error occurred: " +
+            textStatus, errorThrown
+        );
+    });
+
+    // // Callback handler that will be called regardless
+    // // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+
+    // // Prevent default posting of form
+    // event.preventDefault();
+});
